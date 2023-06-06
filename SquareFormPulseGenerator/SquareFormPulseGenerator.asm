fda3db55372ce88a399d8fbaf48a34ccd62700b4
unknown <hellhoroka@yandex.ru>
2023-06-06 15:25:34 +0300

    Финалочка по программе

--- SquareFormPulseGenerator/SquareFormPulseGenerator.asm
+++ SquareFormPulseGenerator/SquareFormPulseGenerator.asm
@@ -4,8 +4,8 @@ RomSize    EQU   4096
 
 			MatrixPowerPortL = 0FBh; Матрицы 0-7
 			MatrixPowerPortH = 0F7h; Матрицы 8-15
-			MatrixColumnPort = 0FEh;
-			MatrixRowPort = 0FDh;
+			MatrixColumnPort = 0FEh; 
+			MatrixRowPort = 0FDh; 
 			DisplayPowerPort = 0EFh ;0-2 Частота, 3 Амплитуда, 4 Длительность импульса
 			DisplaySegmentsPort = 0DFh
 			KeyboardPort = 0FEh
@@ -19,77 +19,64 @@ IntTable   ENDS
 Data       SEGMENT use16 AT 40h
 ;Здесь размещаются описания переменных
 
-			FrequancyBCD db 2 dup(?)
-			FrequancyImage db 3 dup(?)
-
-			Frequancy db ?
-			Amplitude db ?
-			PulseDuration db ?
-			PauseDuration db ?
-			PulsePeriod db ?
-
-			NoInputErrorFlag db ?
-
-			PolarityFlag db ?
-
+			FrequancyBCD db 2 dup(?) ;Частота в десятичном коде
+			FrequancyImage db 3 dup(?) ;Частота в десятичном коде в распакованном формате
+			Frequancy db ? ;Частота
+			Amplitude db ? ;Амплитуда
+			PulseDuration db ? ;Длительность импульса
+			PauseDuration db ? ;Длительность паузы
+			PulsePeriod db ? ;Период
+			NoInputErrorFlag db ? ;Фгал неактивных кнопок
+			PolarityFlag db ? ;Флаг полярности
 			DataHexArr db 10 dup(?)
 			DataHexTabl db 10 dup(?)
-
 			KeyImage db ? ; FF: ничего, FE: + Freq, FD: - Freq, FB: + Ampl, F7: - Ampl, EF: + PulDur, DF: - PulDur, BF: Generation, 7F: Polarity
-			OldButton db ?
-
-			PulsesImage db 128 dup(?)
-
-			PulsesCount db ?
-
-			StartPosition db ?
-
-
-
+			OldButton db ? ; Предыдущее состояние кнопки
+			PulsesImage db 128 dup(?) ;Массив отображения импульсов
+			PulsesCount db ? ;Количесвто импульсов
+			StartPosition db ? ;Начальная позиция указателя
 
 Data       ENDS
 
 ;Задайте необходимый адрес стека
-Stack        SEGMENT use16 AT 2000h
+Stk			SEGMENT use16  AT 00FFh
 ;Задайте необходимый размер стека
            dw    16 dup (?)
 StackTop     Label Word
-Stack        ENDS
+Stk        ENDS
 
 InitData   SEGMENT use16
 InitDataStart:
 ;Здесь размещаются описания констант
-
-
-
 InitDataEnd:
 InitData   ENDS
 
 Code       SEGMENT use16
 ;Здесь размещаются описания констант
 
-			ASSUME cs:Code,ds:Data,es:Data
+			ASSUME cs:Code,ds:Data,es:Data, ss:Stk
 			HexArr DB 00h,01h,02h,03h,04h,05h,06h,07h,08h,09h
-			HexTabl DB 3Fh,0Ch,76h,5Eh,4Dh,5Bh,7Bh,0Eh,7Fh,5Fh
+			HexTabl DB 0C0h,0F3h,89h,0A1h,0B2h,0A4h,84h,0F1h,80h,0A0h
 			
-Initialization PROC 
+Initialization PROC ;Функциональная подготовка
 			CALL CopyArraysToDataSegment
 			XOR AX, AX
 			MOV Frequancy, 20
+			MOV Amplitude, 5; 
+			MOV PulseDuration, 5
+			MOV KeyImage, 0FFh
+			MOV NoInputErrorFlag, 0FFh
+			MOV PolarityFlag, AH
 			MOV FrequancyImage+0, AH
 			MOV FrequancyImage+1, AH
 			MOV FrequancyImage+2, AH
 			MOV FrequancyBCD+0, AH
 			MOV FrequancyBCD+1, AH
 			MOV FrequancyBCD+2, AH
-			MOV Amplitude, 5; Амплитуда = 0
-			MOV PulseDuration, 5
 			MOV PulsePeriod, AH
-			MOV NoInputErrorFlag, 0FFh
-			MOV OldButton, AH
-			MOV KeyImage, 0FFh
 			MOV PulsesCount, AH
-			MOV PolarityFlag, AH
+			MOV PauseDuration, AH
+			MOV OldButton, AH
 			LEA DI, PulsesImage
 			MOV CX, 128
 M1:			MOV [DI], AL
@@ -98,68 +85,6 @@ M1:			MOV [DI], AL
 			
 			RET
 Initialization ENDP
-
-Delay PROC NEAR
-
-Delay ENDP
-		   
-DisplayFrequancy     PROC  ;Вывод частоты на дисплей
-			LEA BX, DataHexTabl 
-            MOV AH, FrequancyImage+0
-            MOV AL, AH               ;теперь в al старшая цифра
-            XLAT
-		    NOT AL		   ;табличное преобразование старшей цифры
-            OUT DisplaySegmentsPort, AL    ;выводим на страший индикатор
-            MOV AL, 1            
-            OUT DisplayPowerPort, AL    ;зажигаем старший индикатор    
-            MOV AL,00h             
-            OUT DisplayPowerPort, AL    ;гасим индикатор
-		    MOV AH, FrequancyImage+1      ;загружаем в регистры
-            MOV AL, AH              ;текущее значение суммы                 
-            XLAT
-		    NOT AL ;табличное преобразование младшей цифры
-            OUT DisplaySegmentsPort, AL    ;Выводим на младший индикатор            
-            MOV AL, 2            
-            OUT DisplayPowerPort, AL    ;зажигаем младший индикатор
-            MOV AL,00h
-            OUT DisplayPowerPort, AL    ;гасим индикатор
-			MOV AH, FrequancyImage+2      ;загружаем в регистры
-            MOV AL, AH              ;текущее значение суммы                 
-            XLAT
-		    NOT AL ;табличное преобразование младшей цифры
-            OUT DisplaySegmentsPort, AL    ;Выводим на младший индикатор            
-            MOV AL, 4            
-            OUT DisplayPowerPort, AL    ;зажигаем младший индикатор
-            MOV AL,00h
-            OUT DisplayPowerPort, AL    ;гасим индикатор
-            RET
-DisplayFrequancy     ENDP
-
-DisplayAmplitude     PROC  ;Вывод амплитуды на дисплей
-			LEA BX, DataHexTabl 
-            MOV AL, Amplitude
-            XLAT
-		    NOT AL		   ;табличное преобразование старшей цифры
-            OUT DisplaySegmentsPort, AL    ;выводим на страший индикатор
-            MOV AL, 8            
-            OUT DisplayPowerPort, AL    ;зажигаем старший индикатор    
-            MOV AL,00h             
-            OUT DisplayPowerPort, AL    ;гасим индикатор
-            RET
-DisplayAmplitude     ENDP
-
-DisplayPulseDuration     PROC  ;Вывод длительности импульса на дисплей
-			LEA BX, DataHexTabl 
-            MOV AL, PulseDuration
-            XLAT
-		    NOT AL		   ;табличное преобразование старшей цифры
-            OUT DisplaySegmentsPort, AL    ;выводим на страший индикатор
-            MOV AL, 16            
-            OUT DisplayPowerPort, AL    ;зажигаем старший индикатор    
-            MOV AL,00h             
-            OUT DisplayPowerPort, AL    ;гасим индикатор
-            RET
-DisplayPulseDuration     ENDP
 
 CopyArraysToDataSegment PROC 
 			MOV CX, 10 ;Загрузка счётчика циклов
@@ -185,22 +110,57 @@ M1:
 			RET
 CopyArraysToDataSegment ENDP
 
-PulsePeriodAndPauseCalculation PROC  
-			MOV AX, MS
-			DIV Frequancy
-			MOV PulsePeriod, AL
-			SUB AL, PulseDuration
-			MOV PauseDuration, AL
+KeyRead    PROC   ;Чтение кнопок
+			MOV DX, KeyboardPort
+            IN AL, KeyboardPort
+			CALL VibrDestr
+            MOV AH, AL
+            XOR AL, OldButton
+            AND AL, AH
+			NOT AL
+			MOV OldButton, AH
+			MOV KeyImage, AL		
 			RET
-PulsePeriodAndPauseCalculation ENDP
+KeyRead    ENDP
 
-PulsesCountCalculation PROC 
-			XOR AX, AX
-			MOV AL, LENGTH PulsesImage
-			DIV PulsePeriod
-			MOV PulsesCount, AL
+VibrDestr  PROC  
+VD1:        mov   ah,al       ;Сохранение исходного состояния
+            mov   bh,0        ;Сброс счётчика повторений
+VD2:        in    al,dx       ;Ввод текущего состояния
+            cmp   ah,al       ;Текущее состояние=исходному?
+            jne   VD1         ;Переход, если нет
+            inc   bh          ;Инкремент счётчика повторений
+            cmp   bh,NMax     ;Конец дребезга?
+            jne   VD2         ;Переход, если нет
+            mov   al,ah       ;Восстановление местоположения данных
+            ret
+VibrDestr  ENDP
+
+KeyCheck PROC 
+			CMP KeyImage, 0FFh
+			JNZ M1
+			MOV NoInputErrorFlag, 0FFh
+			JMP M2
+M1:			MOV NoInputErrorFlag, 00h
+M2:			RET
+KeyCheck ENDP
+
+DataSetting PROC 
+			CMP NoInputErrorFlag, 0FFh
+			JZ M1
+			CALL FrequancyAddition
+			CALL FrequancySubtraction
+			CALL AmplitudeAddition
+			CALL AmplitudeSubtraction
+			CALL PulseDurationAddition
+			CALL PulseDurationSubtraction
+			CALL PolaritySetting
+			CALL PulsePeriodAndPauseCalculation
+			CALL PulsesCountCalculation
+			CALL StartPositionSetting
+M1:			
 			RET
-PulsesCountCalculation ENDP
+DataSetting ENDP
 
 FrequancyAddition PROC 
 			CMP KeyImage, 0FEh
@@ -256,6 +216,40 @@ PulseDurationSubtraction PROC
 M1:			RET
 PulseDurationSubtraction ENDP
 
+PolaritySetting PROC 
+			CMP KeyImage, 0BFh
+			JNZ M1
+			NOT PolarityFlag
+M1:			RET
+PolaritySetting ENDP
+
+PulsePeriodAndPauseCalculation PROC  
+			MOV AX, MS
+			DIV Frequancy
+			MOV PulsePeriod, AL
+			SUB AL, PulseDuration
+			MOV PauseDuration, AL
+			RET
+PulsePeriodAndPauseCalculation ENDP
+
+PulsesCountCalculation PROC 
+			XOR AX, AX
+			MOV AL, LENGTH PulsesImage
+			DIV PulsePeriod
+			MOV PulsesCount, AL
+			RET
+PulsesCountCalculation ENDP
+
+StartPositionSetting PROC 
+			CMP PolarityFlag, 00h
+			JNZ M1
+			MOV StartPosition, 20h
+			JMP M2
+M1:			MOV StartPosition, 04h
+M2:			
+			RET
+StartPositionSetting ENDP
+
 BinaryToBCD PROC 
 			XOR BX, BX
 			MOV FrequancyBCD+0, BL
@@ -291,67 +285,6 @@ UnpackFrequancyBCD PROC
 			RET
 UnpackFrequancyBCD ENDP
 
-KeyRead    PROC   ;Чтение кнопок
-			MOV DX, KeyboardPort
-            IN AL, KeyboardPort
-			CALL VibrDestr
-            MOV AH, AL
-            XOR AL, OldButton
-            AND AL, AH
-			NOT AL
-			MOV OldButton, AH
-			MOV KeyImage, AL
-            
-						
-			RET
-KeyRead    ENDP
-
-KeyCheck PROC 
-			CMP KeyImage, 0FFh
-			JNZ M1
-			MOV NoInputErrorFlag, 0FFh
-			JMP M2
-M1:			MOV NoInputErrorFlag, 00h
-M2:			RET
-KeyCheck ENDP
-
-VibrDestr  PROC  
-VD1:        mov   ah,al       ;Сохранение исходного состояния
-            mov   bh,0        ;Сброс счётчика повторений
-VD2:        in    al,dx       ;Ввод текущего состояния
-            cmp   ah,al       ;Текущее состояние=исходному?
-            jne   VD1         ;Переход, если нет
-            inc   bh          ;Инкремент счётчика повторений
-            cmp   bh,NMax     ;Конец дребезга?
-            jne   VD2         ;Переход, если нет
-            mov   al,ah       ;Восстановление местоположения данных
-            ret
-VibrDestr  ENDP
-
-PolaritySetting PROC 
-			CMP KeyImage, 0BFh
-			JNZ M1
-			NOT PolarityFlag
-M1:			RET
-PolaritySetting ENDP
-
-DataSetting PROC 
-			CMP NoInputErrorFlag, 0FFh
-			JZ M1
-			CALL FrequancyAddition
-			CALL FrequancySubtraction
-			CALL AmplitudeAddition
-			CALL AmplitudeSubtraction
-			CALL PulseDurationAddition
-			CALL PulseDurationSubtraction
-			CALL PolaritySetting
-			CALL PulsePeriodAndPauseCalculation
-			CALL PulsesCountCalculation
-			CALL StartPositionSetting
-M1:			
-			RET
-DataSetting ENDP
-
 PulseImageForming PROC 
 			LEA BX, PulsesImage
 			XOR DI, DI
@@ -386,14 +319,15 @@ XAxisForming PROC
 			CMP PolarityFlag, 0FFh
 			JNZ M1
 			SHR AH, 2
-			CMP Amplitude, 00h
+M3:			CMP Amplitude, 00h
 			JNZ M2
 			OR AH, AL
 			JMP M2
 			
 M1:			SHL AH, 2
-			CMP Amplitude, 00h
-			JNZ M2
+			JMP M3
+			;CMP Amplitude, 00h
+			;JNZ M2
 			OR AH, AL
 
 M2:			OR [BX+DI], AH
@@ -403,7 +337,6 @@ M2:			OR [BX+DI], AH
 
 			RET
 XAxisForming ENDP
-
 
 AmplitudeUpImageForming PROC 
 			MOV CL, Amplitude
@@ -459,6 +392,66 @@ M4:			CMP DI, 128
 M7:			RET
 PauseDurationImageForming ENDP
 
+DisplayData PROC 
+			CALL DisplayFrequancy
+			CALL DisplayAmplitude
+			CALL DisplayPulseDuration
+			RET
+DisplayData ENDP
+		   
+DisplayFrequancy     PROC  ;Вывод частоты на дисплей
+			LEA BX, DataHexTabl 
+            MOV AH, FrequancyImage+0
+            MOV AL, AH               ;теперь в al старшая цифра
+            XLAT			   ;табличное преобразование старшей цифры
+            OUT DisplaySegmentsPort, AL    ;выводим на страший индикатор
+            MOV AL, 1            
+            OUT DisplayPowerPort, AL    ;зажигаем старший индикатор    
+            MOV AL,00h             
+            OUT DisplayPowerPort, AL    ;гасим индикатор
+		    MOV AH, FrequancyImage+1      ;загружаем в регистры
+            MOV AL, AH              ;текущее значение суммы                 
+            XLAT 			;табличное преобразование младшей цифры
+            OUT DisplaySegmentsPort, AL    ;Выводим на младший индикатор            
+            MOV AL, 2            
+            OUT DisplayPowerPort, AL    ;зажигаем младший индикатор
+            MOV AL,00h
+            OUT DisplayPowerPort, AL    ;гасим индикатор
+			MOV AH, FrequancyImage+2      ;загружаем в регистры
+            MOV AL, AH              ;текущее значение суммы                 
+            XLAT			;табличное преобразование младшей цифры
+            OUT DisplaySegmentsPort, AL    ;Выводим на младший индикатор            
+            MOV AL, 4            
+            OUT DisplayPowerPort, AL    ;зажигаем младший индикатор
+            MOV AL,00h
+            OUT DisplayPowerPort, AL    ;гасим индикатор
+            RET
+DisplayFrequancy     ENDP
+
+DisplayAmplitude     PROC  ;Вывод амплитуды на дисплей
+			LEA BX, DataHexTabl 
+            MOV AL, Amplitude
+            XLAT 							;табличное преобразование старшей цифры
+            OUT DisplaySegmentsPort, AL    ;выводим на страший индикатор
+            MOV AL, 8            
+            OUT DisplayPowerPort, AL    ;зажигаем старший индикатор    
+            MOV AL,00h             
+            OUT DisplayPowerPort, AL    ;гасим индикатор
+            RET
+DisplayAmplitude     ENDP
+
+DisplayPulseDuration     PROC  ;Вывод длительности импульса на дисплей
+			LEA BX, DataHexTabl 
+            MOV AL, PulseDuration
+            XLAT		   ;табличное преобразование старшей цифры
+            OUT DisplaySegmentsPort, AL    ;выводим на страший индикатор
+            MOV AL, 16            
+            OUT DisplayPowerPort, AL    ;зажигаем старший индикатор    
+            MOV AL,00h             
+            OUT DisplayPowerPort, AL    ;гасим индикатор
+            RET
+DisplayPulseDuration     ENDP
+
 MatrixOutput PROC 
 			LEA SI, PulsesImage
 			MOV DL, MatrixPowerPortL
@@ -470,8 +463,6 @@ M3:			PUSH CX
 			MOV CX, 8
 			
 M2:			PUSH CX
-			MOV AL, BL
-			OUT DX, AL
 			
 			MOV CX, 8
 			
@@ -479,14 +470,15 @@ M1:			MOV AL, [SI]
 			OUT MatrixRowPort, AL
 			MOV AL, AH
 			OUT MatrixColumnPort, AL
+			MOV AL, BL
+			OUT DX, AL
 			MOV AL, 0
 			OUT MatrixRowPort, AL
 			OUT MatrixColumnPort, AL
+			OUT DX, AL 
 			ROL AH, 1
 			INC SI
 			LOOP M1
-			MOV AL, 0
-			OUT DX, AL 
 			ROL BL, 1
 			POP CX
 			LOOP M2
@@ -497,28 +489,11 @@ M1:			MOV AL, [SI]
 			RET
 MatrixOutput ENDP
 
-StartPositionSetting PROC 
-			CMP PolarityFlag, 00h
-			JNZ M1
-			MOV StartPosition, 20h
-			JMP M2
-M1:			MOV StartPosition, 04h
-M2:			
-			RET
-StartPositionSetting ENDP
-
-DisplayData PROC 
-			CALL DisplayFrequancy
-			CALL DisplayAmplitude
-			CALL DisplayPulseDuration
-			RET
-DisplayData ENDP
-
 Start:
             mov   ax,Data
             mov   ds,ax
             mov   es,ax
-            mov   ax,Stack
+            mov   ax,Stk
             mov   ss,ax
             lea   sp,StackTop
 			
@@ -529,9 +504,9 @@ ILOOP:
 			CALL KeyCheck
 			CALL DataSetting
 			CALL BinaryToBCD
-			CALL UnpackFrequancyBCD		
+			CALL UnpackFrequancyBCD	
+			CALL PulseImageForming			
 			CALL DisplayData
-			CALL PulseImageForming
 			CALL MatrixOutput
 			JMP ILOOP
 
